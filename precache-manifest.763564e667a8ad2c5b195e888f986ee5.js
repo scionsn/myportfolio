1109efbf126f64d7c5c2d774e968dace4c268216
scionsn <52966844+scionsn@users.noreply.github.com>
2020-09-04 13:38:59 +0530

    Updates

---
+++ precache-manifest.763564e667a8ad2c5b195e888f986ee5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "825f6c9f3cb9da9ed8b0dcee8ed4fbdf",
+    "url": "/myportfolio/index.html"
+  },
+  {
+    "revision": "8bef5faf8471d977c14a",
+    "url": "/myportfolio/static/css/2.3a7e2fab.chunk.css"
+  },
+  {
+    "revision": "917150565967478e003c",
+    "url": "/myportfolio/static/css/main.89076365.chunk.css"
+  },
+  {
+    "revision": "8bef5faf8471d977c14a",
+    "url": "/myportfolio/static/js/2.ba3b0f88.chunk.js"
+  },
+  {
+    "revision": "ab3ed49c9114327d36bee72995bc6076",
+    "url": "/myportfolio/static/js/2.ba3b0f88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "917150565967478e003c",
+    "url": "/myportfolio/static/js/main.118ac7d4.chunk.js"
+  },
+  {
+    "revision": "da2e02db1e870a8bb8e5",
+    "url": "/myportfolio/static/js/runtime-main.b5f0a273.js"
+  }
+]);
